5ea390290b7ca8c3c27fa5d4e231d3899b05de0c
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-11-12 04:36:41 +0900

    initial commit

---
+++ TableViewCheckMarkSampleTests/TableViewCheckMarkSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  TableViewCheckMarkSampleTests.swift
+//  TableViewCheckMarkSampleTests
+//
+//  Created by RS on 2016/11/11.
+//  Copyright © 2016年 RS. All rights reserved.
+//
+
+import XCTest
+@testable import TableViewCheckMarkSample
+
+class TableViewCheckMarkSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
